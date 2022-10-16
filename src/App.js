673c1d42af2c6bc58f13ee7942cf6c4a2fc6d7d6
angelicapedroso <angelicapedroso05@gmail.com>
2022-10-16 16:32:02 -0300

    feat: adiciona rotas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
     <div className="App">
-      <h1>Blog Box</h1>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
