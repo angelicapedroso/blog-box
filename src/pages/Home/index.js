adef324b17c379d7ed7384168b237fe53c3fc1e6
angelicapedroso <angelicapedroso05@gmail.com>
2022-10-22 17:11:46 -0300

    feat: adiciona estrutura da pagina home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import './styles.module.css';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './styles.module.css';
 
 function Home() {
+  const [query, setQuery] = useState('');
+  const [posts] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>Vejo os posts mais recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input
+          type="text"
+          placeholder="Ou faça uma busca por tags..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit" className="btn btn-dark">Buscar</button>
+      </form>
+      <div>
+        <h1>Posts</h1>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Nenhum post encontrado</p>
+            <Link to="/posts/create">Criar um post</Link>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
 
