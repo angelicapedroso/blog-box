4a7129327a36a83a7d78ac3e8f355a8b38a6d3a4
angelicapedroso <angelicapedroso05@gmail.com>
2022-10-23 12:30:13 -0300

    feat: exibe resultados da busca

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './styles.module.css';
-// import useFetchDocuments from '../../hooks/useFetchDocuments';
+import useFetchDocuments from '../../hooks/useFetchDocuments';
 import useQuery from '../../hooks/useQuery';
+import PostDetail from '../../components/PostDetail';
 
 function Search() {
   const query = useQuery();
   const search = query.get('q');
 
+  const { documents: posts } = useFetchDocuments('posts', search);
+
   return (
     <div className={styles.search}>
-      <p>{search}</p>
+      <div>
+        {posts && posts.length === 0 && (
+          <div>
+            <p>Não foi encontrado nenhum post com a sua busca...</p>
+            <Link to="/" className="btn btn-light">Voltar</Link>
+          </div>
+        )}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+      </div>
     </div>
   );
 }
