b977437d3194a334812403bdfe2b1416a1225aa2
angelicapedroso <angelicapedroso05@gmail.com>
2022-10-23 13:47:16 -0300

    feat: exibe informacoes sobre o post

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -8,11 +8,22 @@ function Post() {
   const { document: post, loading } = useFetchDocument('posts', id);
 
   return (
-    <div className={styles.post}>
+    <div className={styles.post_container}>
       {loading && <p>Carregando...</p>}
       {post && (
         <div>
           <h2>{post.title}</h2>
+          <img src={post.image} alt={post.title} />
+          <p>{post.body}</p>
+          <h3>Este post fala sobre:</h3>
+          <div className={styles.tags}>
+            {post.tags.map((tag) => (
+              <p key={tag}>
+                <span>#</span>
+                {tag}
+              </p>
+            ))}
+          </div>
         </div>
       )}
     </div>
