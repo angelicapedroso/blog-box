d63bd065ba5357183ea3016b5dc37344248f8bc2
angelicapedroso <angelicapedroso05@gmail.com>
2022-10-26 16:17:39 -0300

    feat: estiliza pagina de registro

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import useAuthentication from '../../hooks/useAuthentication';
+import Container from './styles';
 
 function Register() {
   const [name, setName] = useState('');
@@ -36,11 +37,20 @@ function Register() {
     }
   }, [authError]);
 
+  const logo = '< CODEBlog />';
+
   return (
-    <div>
-      <h1>Cadastre-se para começar a postar</h1>
-      <p>Crie sua conta e compartilhe suas ideias e histórias</p>
+    // page login
+    <Container>
+      {/* container-form */}
+      <div className="logo">
+        <h1>{logo}</h1>
+      </div>
       <form onSubmit={handleSubmit}>
+        <div className="title">
+          <h2>Bem-vindx!</h2>
+          <h3>Cadastre-se para começar a postar!</h3>
+        </div>
         <label htmlFor="name">
           <span>Nome:</span>
           <input
@@ -85,7 +95,7 @@ function Register() {
         {loading && <button className="btn" type="submit" disabled>Aguarde...</button>}
         {error && <p className="error">{error}</p>}
       </form>
-    </div>
+    </Container>
   );
 }
 
--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -1,8 +1,50 @@
-.register {
-  text-align: center;
-  color: var(--color-five);
-}
+import styled from 'styled-components';
 
-.register p {
-  color: var(--color-four);
-}
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  height: 89vh;
+  background-color: var(--color-four);
+
+  form {
+    display: flex;
+    flex-direction: column;
+    height: 80%;
+    border-radius: 10px;
+    color: var(--color-five);
+  }
+
+  .logo {
+    display: flex;
+    text-align: center;
+    justify-content: center;
+    width: 50%;
+    height: 100%;
+    background-color: var(--color-one);
+  }
+
+  .logo h1 {
+    color: var(--color-four);
+    font-size: 3em;
+    align-self: center;
+  }
+
+  .title h2 {
+    margin-bottom: 0.1em;
+  }
+
+  .title {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.3em;
+  }
+
+  button {
+    width: 100%;
+  }
+`;
+
+export default Container;
